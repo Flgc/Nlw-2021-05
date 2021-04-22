f3eb9bf72b5b5b1444ea3222b3fd46dbada47e04
Fabio Luis <ml@fabiotecnico1.biz>
2021-04-22 19:27:33 -0300

    Aula 3 - Implementando Axios, formatação de data estilizando a HOME, Roteamento, Página Epsódio

---
+++ src/utils/convertDurationToTimeString.ts
@@ -0,0 +1,11 @@
+export function convertDurationToTimeString(duration: number) {
+  const hours = Math.floor(duration / 3600);
+  const minutes = Math.floor((duration % 3600) / 60);
+  const seconds = (duration = duration % 60);
+
+  const timeString = [hours, minutes, seconds]
+    .map((unit) => String(unit).padStart(2, '0'))
+    .join(':'); // Percorre a unit para que (hora, minutos e segundos tenham sem 2 digitos)
+
+  return timeString;
+}
